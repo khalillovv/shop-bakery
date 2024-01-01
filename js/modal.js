e30a6174044a467176976bf484e7bdd9df2880eb
Alexander <kapriz2k16@gmail.com>
2024-01-01 16:00:52 +0200

    fix modal

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,12 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
+const body = document.body;
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+const toggleModal = () => {
+  modal.classList.toggle('is-hidden');
+  body.classList.toggle('no-scroll');
+};
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
